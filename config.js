36f34e357dd917aab093ea9c27b041b7ecb5f5fb
thelowend <dmpablos@gmail.com>
2020-12-09 17:41:31 -0300

    Remove default keys

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ const config = {
     url: process.env.MONGO_DB_URI || 'mongodb://localhost/withmedb'
   },
   notifications: {
-    appId: process.env.NOTIFICATIONS_APP_ID || 'f9adf46b-d1d5-463f-8858-b934ef8d908c',
-    apiKey: process.env.NOTIFICATIONS_KEY || 'ODE4ZjA2OWUtZWZlNS00YzliLTg2YzctYmUzOWNlODY1NGJm',
-    apiUrl: process.env.NOTIFICATIONS_API || 'https://onesignal.com/api/v1/',
+    appId: process.env.NOTIFICATIONS_APP_ID,
+    apiKey: process.env.NOTIFICATIONS_KEY,
+    apiUrl: process.env.NOTIFICATIONS_API
   }
 }
 
